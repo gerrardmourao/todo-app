e0bfad179720da118d75d5fa0adcd3c055be014b
gerrardmourao <gerardmourao2019@gmail.com>
2023-11-20 17:18:45 -1200

    adiçao dos icones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
@@ -13,4 +15,3 @@ app.get('/', (requisicao, resposta) => {
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
-
