4ba51497afcf2a32733ace5da7262f78eecab5ec
gerrardmourao <gerardmourao2019@gmail.com>
2023-11-30 16:04:44 -1200

    xxx
       cc

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
+
 const exphbs = require("express-handlebars")
+
 
 const app = express()
 
@@ -14,4 +16,5 @@ app.get('/', (requisicao, resposta) => {
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
+
 })
